217ea61a192dedbc393145d7fa74070dc9a3d641
elisbanpaco <paccoacchaelias@gmail.com>
2026-04-05 13:49:09 -0500

    fix algoritmos.py to api/routes

--- api/routes/algoritmos.py
+++ api/routes/algoritmos.py
@@ -1,13 +1,24 @@
 from fastapi import APIRouter
 from schemas.data_models import Datos
 from core.distancia_coseno import distance_cosine
+from core.jaccard import jaccard
 router = APIRouter()
 
 # Ruta para ALGORITMOS
-@router.post("/comparar-textos")
-async def enviarDatos(datos: Datos):
+@router.post("/comparar-textos-distancia-coseno")
+async def enviarDatosDistanciaCoseno(datos: Datos):
 
     result = distance_cosine(datos.textoA, datos.textoB)
+    return {
+        "message": "Datos enviados correctamente",
+        "similarity": float(result)
+        }
+
+
+@router.post("/comparar-textos-jaccard")
+async def enviarDatosJaccard(datos: Datos):
+
+    result = jaccard(datos.textoA, datos.textoB)
     return {
         "message": "Datos enviados correctamente",
         "similarity": float(result)
