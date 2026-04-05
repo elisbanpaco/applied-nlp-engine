a25f9e8b50bed432c5ffb43d7d092b3580918bfd
elisbanpaco <paccoacchaelias@gmail.com>
2026-04-05 13:49:31 -0500

    fix jaccard.py to api/core

--- api/core/jaccard.py
+++ api/core/jaccard.py
@@ -1,3 +1,24 @@
+import spacy
+
+# Cargamos el modelo en español
+nlp = spacy.load("es_core_news_md")
+
+def set_limpio(texto):
+    """
+    Toma un string crudo, lo procesa, elimina stopwords/puntuación
+    y devuelve un 'set' de palabras clave en minúsculas.
+    """
+    # 1. Pasamos el texto a minúsculas (para que 'Python' y 'python' sean iguales)
+    doc = nlp(texto.lower())
+    
+    # 2. Usamos "Set Comprehension" para filtrar y crear el set en una sola línea
+    palabras_limpias = {
+        token.text for token in doc 
+        if not token.is_stop and not token.is_punct and not token.is_space
+    }
+    
+    return palabras_limpias
+
 def similitud_jaccard(conjunto1, conjunto2):
     """
     Calcula el índice de Jaccard entre dos conjuntos de datos.
@@ -16,21 +37,28 @@ def similitud_jaccard(conjunto1, conjunto2):
     # 4. Fórmula Matemática: |A ∩ B| / |A ∪ B|
     return len(interseccion) / len(union)
 
-# --- CASO DE USO REAL: Sistema de Reclutamiento ---
 
-# Usamos 'set' usando las llaves {} en Python
-requisitos_puesto = {"python", "aws", "docker", "linux", "sql"}
+def jaccard(textoA, textoB):
+    conjuntoA = set_limpio(textoA)
+    conjuntoB = set_limpio(textoB)
+    return similitud_jaccard(conjuntoA, conjuntoB)
 
-candidato_ideal = {"python", "aws", "docker", "linux", "sql"}
-candidato_bueno = {"python", "aws", "docker", "html", "css"}
-candidato_malo  = {"javascript", "react", "node", "mongo"}
 
-# Evaluamos
-score_ideal = similitud_jaccard(requisitos_puesto, candidato_ideal)
-score_bueno = similitud_jaccard(requisitos_puesto, candidato_bueno)
-score_malo  = similitud_jaccard(requisitos_puesto, candidato_malo)
 
-print("=== RESULTADOS DEL MATCHING (JACCARD) ===")
-print(f"Candidato Ideal: {score_ideal:.2f} (Match del {score_ideal*100}%)")
-print(f"Candidato Bueno: {score_bueno:.2f} (Match del {score_bueno*100}%)")
-print(f"Candidato Malo : {score_malo:.2f} (Match del {score_malo*100}%)")
+
+# # Usamos 'set' usando las llaves {} en Python
+# requisitos_puesto = {"python", "aws", "docker", "linux", "sql"}
+
+# candidato_ideal = {"python", "aws", "docker", "linux", "sql"}
+# candidato_bueno = {"python", "aws", "docker", "html", "css"}
+# candidato_malo  = {"javascript", "react", "node", "mongo"}
+
+# # Evaluamos
+# score_ideal = similitud_jaccard(requisitos_puesto, candidato_ideal)
+# score_bueno = similitud_jaccard(requisitos_puesto, candidato_bueno)
+# score_malo  = similitud_jaccard(requisitos_puesto, candidato_malo)
+
+# print("=== RESULTADOS DEL MATCHING (JACCARD) ===")
+# print(f"Candidato Ideal: {score_ideal:.2f} (Match del {score_ideal*100}%)")
+# print(f"Candidato Bueno: {score_bueno:.2f} (Match del {score_bueno*100}%)")
+# print(f"Candidato Malo : {score_malo:.2f} (Match del {score_malo*100}%)")
